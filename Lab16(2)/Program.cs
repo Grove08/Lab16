866cfcefd24609316b65293692d2088c2a6bcaf8
Артём Белоусов <belousov@psksk.ru>
2024-02-16 15:15:49 +0300

    Добавьте файлы проекта.

---
+++ Lab16(2)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Xml.Schema;
+
+namespace Lab16_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string jsonString = String.Empty;
+            using (StreamReader sr = new StreamReader("../../../Products.json"))
+            {
+                jsonString = sr.ReadToEnd();
+            }
+            Product[] products = JsonSerializer.Deserialize<Product[]>(jsonString);
+
+            Product mostExpensiveProduct = products[0];
+            foreach (Product e in products)
+            {
+                if (e.Price > mostExpensiveProduct.Price)
+                {
+                    mostExpensiveProduct = e;
+                }
+            }
+            Console.WriteLine($"{mostExpensiveProduct.Code} {mostExpensiveProduct.Name} {mostExpensiveProduct.Price}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab16/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Encodings.Web;
+using System.Text.Unicode;
+using System.Text.Json.Serialization;
+using System.IO;
+
+namespace Lab16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int n = 5;
+            Product[] products= new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {          
+
+                Console.WriteLine("Код товара");
+                int code = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Название товара");
+                string name = Console.ReadLine();
+                Console.WriteLine("Цена товара");
+                double price = Convert.ToDouble(Console.ReadLine());
+                products[i] = new Product() {Code = code, Name = name, Price = price};
+            }
+
+
+            string jsonString = JsonSerializer.Serialize(products);
+
+            using (StreamWriter sw = new StreamWriter("../../../Products.json"))
+            {
+                sw.WriteLine(jsonString); 
+            }
+        }
+    }
+}
